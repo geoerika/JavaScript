fc60bc6fbab328184f9775eeef173062bd226ccf
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-07 21:32:01 +0000

    implemented pre_order traverse of a binary tree

---
+++ trees/trees_pre_order_traversal.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    if (this.root === null) {
+      this.root = new Node(key);
+    } else {
+        this.insertNode(this.root, key);
+    }
+  }
+
+  insertNode(node, key) {
+    if (key < node.key) {
+      if (node.left === null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = new Node (key);
+      } else {
+        this.insertNode(node.right, key);
+      }
+    }
+  }
+
+  preOrderTraverse(callback) {
+    this.preOrderTraverseNode(this.root, callback);
+  }
+
+  preOrderTraverseNode(node, callback) {
+    if (node != null) {
+      callback(node.key);
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(9);
+tree.insert(20);
+tree.insert(13);
+tree.insert(2);
+tree.insert(17);
+tree.insert(1);
+tree.insert(19);
+
+const printNode = (value) => console.log(value);
+tree.preOrderTraverse(printNode);
