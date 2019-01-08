6ff5d573647b5f710eb28122dc981bcf9bda2f8c
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-08 00:18:02 +0000

    implemented search for a value in a binary tree

---
+++ trees/tree_search.js
@@ -0,0 +1,75 @@
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    if (this.root === null) {
+      this.root = new Node(key);
+    } else {
+        this.insertNode(this.root, key);
+    }
+  }
+
+  insertNode(node, key) {
+    if (key < node.key) {
+      if (node.left === null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = new Node (key);
+      } else {
+        this.insertNode(node.right, key);
+      }
+    }
+  }
+
+  search(key) {
+    return this.searchNode(this.root, key);
+  }
+
+  searchNode(node, key) {
+    if (node === null) {
+      return false;
+    }
+
+    if (node.key === key) {
+      return true;
+    }
+
+    if (node.key > key) {
+      return this.searchNode(node.left, key);
+    } else {
+      return this.searchNode(node.right, key);
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(9);
+tree.insert(20);
+tree.insert(13);
+tree.insert(2);
+tree.insert(17);
+tree.insert(1);
+tree.insert(19);
+
+console.log(tree.search(20));
+console.log(tree.search(7));
+console.log(tree.search(6));
