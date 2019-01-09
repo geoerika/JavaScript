22c858323844e50546da99e580aaf710a540b632
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-09 19:04:04 +0000

    created function to generate unsorted array

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,6 +1,6 @@
 const bubbleSort= (array) => {
   const { length} = array;
-  for (let i = 0; i< length; i++) {
+  for (let i = 0; i < length; i++) {
     for (let j = 0; j < length - 1; j++){
       if (array[j] > array[j + 1]) {
         swap(array, j, j + 1);
@@ -17,5 +17,18 @@ const swap = (array, a, b) => {
   [array[a] , array[b]] = [array[b], array[a]];
 }
 
+const createNonSortedArray = (size) => {
+  const array = [];
+  for (let i = size; i > 0; i--) {
+    array.push(i);
+  }
+  return array;
+}
+
 let array1 = [3, 6, 4, 2, 10, 1, 0, 5];
 console.log(bubbleSort(array1));
+
+let array = createNonSortedArray(10);
+console.log(array.join());
+array = bubbleSort(array);
+console.log(array.join());
