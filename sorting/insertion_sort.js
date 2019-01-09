3ab4f328c5d8fb27e6e49cd825db198993e881ec
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-09 20:44:18 +0000

    implemented insertion sorting of an array

---
+++ sorting/insertion_sort.js
@@ -0,0 +1,31 @@
+const insertionSort = (array) => {
+  const {length} = array;
+  for (let i = 1; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[i] < array[j]) {
+        swap(array, i, j);
+      }
+    }
+  }
+  return array;
+}
+
+const swap = (array, a, b) => {
+  [array[a] , array[b]] = [array[b], array[a]];
+}
+
+const createNonSortedArray = (size) => {
+  const array = [];
+  for (let i = size; i > 0; i--) {
+    array.push(i);
+  }
+  return array;
+}
+
+let array1 = [6, 3, 0, 2, 7, 5, 9];
+console.log(insertionSort(array1));
+
+let array = createNonSortedArray(10);
+console.log(array.join());
+array = insertionSort(array);
+console.log(array.join());
