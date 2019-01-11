7260c980884dccec4cc2f5f25c11b8545f325e19
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-11 22:39:05 +0000

    implemented a counting sort for an array of integers

--- sorting/counting_sort.js
+++ sorting/counting_sort.js
@@ -1,6 +1,33 @@
+const countingSort = (array) => {
+
+  if (array.length < 2) {
+    return array;
+  }
+
+  const maxValue = findMaxValue(array);
+  const counts = new Array(maxValue + 1); //declares an array of value counts
+
+  array.forEach(element => {
+    if(!counts[element]) {
+      counts[element] = 0;
+    }
+    counts[element]++;
+  });
+
+  let sortedIndex = 0;
+  counts.forEach((count, i) => {
+    while(count > 0) {
+      array[sortedIndex++] = i;
+      count--;
+    }
+  });
+
+  return array;
+}
+
 const findMaxValue = (array) => {
   let max = array[0];
-  for (let i = o; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (max < array[i]) {
       max = array[i];
     }
@@ -8,3 +35,6 @@ const findMaxValue = (array) => {
 
   return max;
 }
+
+let array1 = [6, 3, 0, 2, 7, 5, 9];
+console.log(countingSort(array1));
