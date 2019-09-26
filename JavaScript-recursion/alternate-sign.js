1a450c07bab78e769a293f3192ec5bc7a10e2bfa
Erika Szabo <erika.szabo.geo@gmail.com>
2019-09-26 19:25:26 -0400

    returns an array with alternate signs

---
+++ JavaScript-recursion/alternate-sign.js
@@ -0,0 +1,21 @@
+// returns alternate sign array
+
+const altSign = (arr) => {
+  let altSignArr = [];
+  if(arr.length < 2) {
+    return altSignArr = arr;
+  }
+  let length = arr.length;
+  let elem = arr.pop(arr[length - 1]);
+  if(length % 2 === 0) {
+    altSignArr = altSign(arr)
+    altSignArr.push(-Math.abs(elem));
+  } else {
+    altSignArr = altSign(arr);
+    altSignArr.push(Math.abs(elem));
+  }
+  return altSignArr;
+}
+
+console.log(altSign([]));
+console.log(altSign([1,2,3,5,6]));
